a09797b6174c3e174cd1d2d5fba43dcb11b63c4f
Cojavim <jajsemtomas@gmail.com>
2021-06-26 21:16:16 +0200

    Initial dist subtree commit

---
+++ dist/precache-manifest.1dbefc027431719ce16b791706adf8d9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05c94b65dc0eea21b17f",
+    "url": "/my-first-project/css/app.4d7b33e6.css"
+  },
+  {
+    "revision": "b4b3372130505b393ca6229bc825ec53",
+    "url": "/my-first-project/index.html"
+  },
+  {
+    "revision": "519c3c3a345649190aa0",
+    "url": "/my-first-project/js/about.7bb8eeb6.js"
+  },
+  {
+    "revision": "05c94b65dc0eea21b17f",
+    "url": "/my-first-project/js/app.4a20f870.js"
+  },
+  {
+    "revision": "07177c99f24b2ffa6380",
+    "url": "/my-first-project/js/chunk-vendors.e2eb090a.js"
+  },
+  {
+    "revision": "e68e7ea6b1f29b9746719df49f50f883",
+    "url": "/my-first-project/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/my-first-project/robots.txt"
+  }
+]);
